748c05c7fdf766313d5ba3fefaa82b2e159774ba
vineStack <perez.lopez.jesus.eduardo0@gmail.com>
2026-03-23 02:03:24 -0600

    footer desktopdesign added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+import Logo from '../../public/CDE Grupo Legislación.png'
+
+const Footer = () => {
+
+    const { t, i18n} = useTranslation();
+
+  return (
+    <footer className='bg-fourth-400 text-white'>
+        <div className='max-w-7xl px-10 py-6 mx-auto gap-16 grid grid-cols-4'>
+
+            <div className='flex flex-col'>
+                <div>
+                    <img src={Logo} alt='CDE Logo Legislacion' className='h-[132px] w-auto object-contain'/>
+                </div>
+                <p className='py-1'>{t('footer.banner-text')}</p>
+            </div>
+
+            <div className='flex flex-col'>
+                <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.enterprise')} </p>
+                <p className='py-1'>{t('footer.about-us')}</p>
+                <p className='py-1'>{t('footer.services')}</p>
+            </div>
+
+            <div className='flex flex-col'>
+                <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.services')} </p>
+                <p className='py-1'>{t('footer.regulatory-affairs')}</p>
+                {/* Por agregar 'Licencias y permisos'*/}
+                <p className='py-1'> {t('footer.auditories')} </p>
+                <p className='py-1'> {t('footer.capacitacion')} </p>
+            </div>
+
+            <div className='flex flex-col'>
+                <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.contact')} </p>
+                <p className='flex gap-2'> <FaPhone className='my-1 text-tertiary-400' /> {t('footer.number')} </p>
+                <p className='flex gap-2'> <MdEmail className='my-1 text-tertiary-400' /> {t('footer.email')}</p>
+            </div>
+
+        </div>
+
+        <div className='max-w-7xl mx-auto px-8 grid grid-cols-3 text-neutral-500 border-t border-t-[1px] border-t-neutral-500'>
+            <p> {t('footer.rights')} </p>
+            <p className='justify-self-end'> {t('footer.privacy')} </p>
+            <p className='justify-self-end'> {t('footer.terms')} </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
