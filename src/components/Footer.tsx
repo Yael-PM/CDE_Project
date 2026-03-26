fb05f5bd5c2a0d05f0845cf32af956cdd059cd3b
vineStack <perez.lopez.jesus.eduardo0@gmail.com>
2026-03-26 14:34:11 -0600

    color changed, hover added, link to about-us adde

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,10 @@ import Logo from '../../public/CDE Grupo Legislación.png'
 
 const Footer = () => {
 
-    const { t, i18n} = useTranslation();
+    const { t} = useTranslation();
 
   return (
-    <footer className='bg-fourth-400 text-white'>
+    <footer className='bg-fourth-500 text-neutral-50 mt-10'>
         <div className='max-w-7xl px-10 py-6 mx-auto gap-16 grid grid-cols-4'>
 
             <div className='flex flex-col'>
@@ -23,16 +23,17 @@ const Footer = () => {
 
             <div className='flex flex-col'>
                 <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.enterprise')} </p>
-                <p className='py-1'>{t('footer.about-us')}</p>
-                <p className='py-1'>{t('footer.services')}</p>
+                <p className='hover:text-fourth-200 py-1'><Link to={'/about-us'}>{t('footer.about-us')}</Link></p>
+                {}
+                <p className='hover:text-fourth-200 py-1'><Link to={'/'}>{t('footer.services')}</Link></p>
             </div>
 
             <div className='flex flex-col'>
                 <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.services')} </p>
-                <p className='py-1'>{t('footer.regulatory-affairs')}</p>
-                {/* Por agregar 'Licencias y permisos'*/}
-                <p className='py-1'> {t('footer.auditories')} </p>
-                <p className='py-1'> {t('footer.capacitacion')} </p>
+                <p className='hover:text-fourth-200 py-1'>{t('footer.regulatory-affairs')}</p>
+                <p className='hover:text-fourth-200 py-1'>{t('footer.sanitary-legislation')}</p>
+                <p className='hover:text-fourth-200 py-1'> {t('footer.auditories')} </p>
+                <p className='hover:text-fourth-200 py-1'> {t('footer.capacitacion')} </p>
             </div>
 
             <div className='flex flex-col'>
@@ -43,8 +44,8 @@ const Footer = () => {
 
         </div>
 
-        <div className='max-w-7xl mx-auto px-8 grid grid-cols-3 text-neutral-500 border-t border-t-[1px] border-t-neutral-500'>
-            <p> {t('footer.rights')} </p>
+        <div className='max-w-7xl mx-auto p-2 flex justify-between text-dark-100 border-t border-t-[1px] border-t-dark-100'>
+            <p className='inline-block'> {t('footer.rights')} </p>
             <p className='justify-self-end'> {t('footer.privacy')} </p>
             <p className='justify-self-end'> {t('footer.terms')} </p>
         </div>
