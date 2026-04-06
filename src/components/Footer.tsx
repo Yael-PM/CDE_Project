6282778701aa4d8f03c95119eeb8f760a54ff8f9
vineStack <perez.lopez.jesus.eduardo0@gmail.com>
2026-04-06 00:56:21 -0600

    margin top removed, link to services added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     const { t} = useTranslation();
 
   return (
-    <footer className='bg-fourth-500 text-neutral-50 mt-10'>
+    <footer className='bg-fourth-500 text-neutral-50'>
         <div className='max-w-7xl px-10 py-6 mx-auto gap-16 grid grid-cols-4'>
 
             <div className='flex flex-col'>
@@ -24,15 +24,15 @@ const Footer = () => {
                 <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.enterprise')} </p>
                 <p className='hover:text-fourth-200 py-1'><Link to={'/about-us'}>{t('footer.about-us')}</Link></p>
                 {/* link to /services remaining */}
-                <p className='hover:text-fourth-200 py-1'><Link to={'/'}>{t('footer.services')}</Link></p>
+                <p className='hover:text-fourth-200 py-1'><Link to={'/services'}>{t('footer.services')}</Link></p>
             </div>
 
             <div className='flex flex-col'>
                 <p className='py-1 text-2xl text-tertiary-400 font-bold text-footer-widgets'> {t('footer.services')} </p>
-                <p className='hover:text-fourth-200 py-1'>{t('footer.regulatory-affairs')}</p>
-                <p className='hover:text-fourth-200 py-1'>{t('footer.sanitary-legislation')}</p>
-                <p className='hover:text-fourth-200 py-1'> {t('footer.auditories')} </p>
-                <p className='hover:text-fourth-200 py-1'> {t('footer.capacitacion')} </p>
+                <p className='hover:text-fourth-200 py-1'> <Link to={'/services'}> {t('footer.regulatory-affairs')}</Link></p>
+                <p className='hover:text-fourth-200 py-1'> <Link to={'/services'}> {t('footer.sanitary-legislation')}</Link></p>
+                <p className='hover:text-fourth-200 py-1'> <Link to={'/services'}> {t('footer.auditories')} </Link></p>
+                <p className='hover:text-fourth-200 py-1'> <Link to={'/services'}> {t('footer.capacitacion')} </Link></p>
             </div>
 
             <div className='flex flex-col'>
